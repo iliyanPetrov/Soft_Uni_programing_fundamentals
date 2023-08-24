12a1845c1234840b3139cbdabf29a5fa2423e986
iliyanPetrov <iliyan.petrov1986@gmail.com>
2023-08-24 23:01:47 +0300

    tested another splice without whileLoop - 85/100

--- 01. Programming Fundamentals Mid Exam Retake/3. MemoryGame.js
+++ 01. Programming Fundamentals Mid Exam Retake/3. MemoryGame.js
@@ -9,7 +9,7 @@ function memoryGame(input) {
         let [index1, index2] = item.split(' ').map(Number);
         // console.log(`item=${item}\n ind1,2=${index1},${index2}\ntype=`, typeof index1)
 
-        if (index1 == index2 || index1<0 || index2>sequence.length || index2<0){
+        if (index1 == index2 || index1<0 || index2>sequence.length || index2<0 || index1>sequence.length){
             console.log("Invalid input! Adding additional elements to the board");
             let additionalElement = `-${moves}a`;
             let middlePoint = Math.floor(sequence.length / 2); // possible fail
@@ -19,35 +19,40 @@ function memoryGame(input) {
             let element1 = sequence[index1];
             let element2 = sequence[index2];
             if ( element1 == element2){
-                console.log(`Congrats! You have found matching elements - ${element1}!`)
-                while (sequence.includes(element1)){ //possible fail
-                    let indexToRemove = sequence.indexOf(element1);
-                    sequence.splice(indexToRemove, 1);
-                }
+                console.log(`Congrats! You have found matching elements - ${element1}!`);
+                // while (sequence.includes(element1)){ //possible fail
+                //     let indexToRemove = sequence.indexOf(element1);
+                //     sequence.splice(indexToRemove, 1);
+                // }
+                sequence.splice(sequence.indexOf(element1), 1);
+                sequence.splice(sequence.indexOf(element2), 1);
                 // console.log('sequence after splice=', sequence)
             } else {
                 console.log("Try again!");
             }
         }
-        // TODO
+
         if (sequence.length==0){
             console.log(`You have won in ${moves} turns!`);
             wonGame = true;
             break;
         }
-
+        if (wonGame)
+            break;
     }
-    if(wonGame) return
-    else {
-        console.log(`Sorry you lose :(\n${sequence.join(' ')}`)
-    }
+    if(wonGame)
+        return '';
+    else 
+        console.log(`Sorry you lose :(\n${sequence.join(' ')}`);
+    
 }
 
 memoryGame([
-    "a 2 4 a 2 4", 
-    "0 3", 
-    "0 2",
-    "0 1",
-    "0 1", 
+    "1 1 2 2 3 3 4 4 5 5", 
+    "1 0", 
+    "-1 0",
+    "1 0",
+    "1 0", 
+    "1 0", 
     "end"
 ]);
