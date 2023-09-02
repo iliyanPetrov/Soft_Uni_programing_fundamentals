0ff8cbe764ccba544c8cc812172a689d427e96cb
iliyanPetrov <iliyan.petrov1986@gmail.com>
2023-09-03 00:55:53 +0300

    3rd task ready

--- 02. Programming Fundamentals Final Exam 09.08.2020/03. Plant Discovery.js
+++ 02. Programming Fundamentals Final Exam 09.08.2020/03. Plant Discovery.js
@@ -12,6 +12,16 @@ function plantDiscovery(input) {
     // console.log(plantsMap, '\ninput=', input)
     for (let line of input) {
         if (line == "Exhibition"){
+            console.log('Plants for the exhibition:');
+            plantsMap.forEach((value, key)=>{
+                let average_rating;
+                if (value['rating'].length == 0){
+                    average_rating = 0 
+                }else{
+                    average_rating = value['rating'].reduce((acc, curr)=>acc+curr, 0)/value['rating'].length
+                }
+                console.log(`- ${key}; Rarity: ${value['rarity']}; Rating: ${average_rating.toFixed(2)}`)
+            })
             break;
         }
         let tokens = line.split(': ');
@@ -24,17 +34,39 @@ function plantDiscovery(input) {
                     console.log("error");
                 }
                 else {
-                    plantsMap.get(plantName)['rating'].push(rating);
-                    console.log('ratingArr=', plantsMap.get(plantName)['rating'])
+                    plantsMap.get(plantName)['rating'].push(+rating);
+                    // console.log('ratingArr=', plantsMap.get(plantName)['rating'])
                 }
-                
             break;
+
             case "Update": 
+                let [plantName1, rarity] = tokens[0].split(' - ');
+                if (!plantsMap.has(plantName1)) {
+                console.log("error");
+                } else {
+                // console.log("old=", plantsMap.get(plantName1)['rarity'])
+                plantsMap.get(plantName1)['rarity'] = +rarity;
+                // console.log("new=", plantsMap.get(plantName1)['rarity'])
+
+                }
             break;
-            case "Reset": 
+
+            case "Reset":
+                let plantName2 = tokens[0];
+                if (!plantsMap.has(plantName2)) {
+                    console.log("error");
+                    } else {
+                    // console.log('old=', plantsMap.get(plantName2)['rating'])
+                    plantsMap.get(plantName2)['rating'] = [];
+                    // console.log('new=', plantsMap.get(plantName2)['rating'])
+                    }
             break;
+            default:
+                console.log('..something went wrong..');
+                break;
         }
     }
+
 }
 
 plantDiscovery([
