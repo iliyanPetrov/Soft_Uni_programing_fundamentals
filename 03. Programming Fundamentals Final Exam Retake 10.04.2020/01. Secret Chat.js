e723c7abb3315a21f03aa37d6139524167c7aa55
iliyanPetrov <iliyan.petrov1986@gmail.com>
2023-09-09 01:56:58 +0300

    2nd task ready at 100%

--- 03. Programming Fundamentals Final Exam Retake 10.04.2020/01. Secret Chat.js
+++ 03. Programming Fundamentals Final Exam Retake 10.04.2020/01. Secret Chat.js
@@ -1,5 +1,46 @@
 function secretChat(input) {
-    let i;
+    let message = input.shift();
+    // console.log(message)
+    for (const line of input) {
+        if(line == "Reveal"){
+            console.log(`You have a new text message: ${message}`)
+            break;
+        }
+        let params = line.split(':|:');
+        let command = params.shift();
+        switch (command) {
+            case "InsertSpace":
+                let index = +params[0];
+                let newMessage = message.split("")
+                newMessage.splice(index, 0, ' ')
+                message = newMessage.join("");
+                console.log(message);
+            break;
+
+            case "Reverse": 
+                let substring = params[0];
+                if (!message.includes(substring)){
+                    console.log("error")
+                }else{
+                    let workMsg = message.replace(substring, "");
+                    let workSubstring = substring.split("").reverse().join("");
+                    message = workMsg.concat(workSubstring)
+                    console.log(message)
+                }
+            break;    
+
+            case "ChangeAll": 
+                let [substring1, replacement] = params;
+                while(message.includes(substring1)){
+                    message = message.replace(substring1, replacement)
+                }
+                console.log(message);
+            break;        
+            default:
+                console.log('something went wrong ...')
+            break;
+        }
+    }
 }
 
 secretChat([
