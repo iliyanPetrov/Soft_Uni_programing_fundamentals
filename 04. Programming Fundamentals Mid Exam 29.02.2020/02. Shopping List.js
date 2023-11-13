9cfa25edfbe0945e9079a02d3d1c9dc7a8087e54
iliyanPetrov <iliyan.petrov1986@gmail.com>
2023-11-14 00:10:42 +0200

    2nd task ready

--- 04. Programming Fundamentals Mid Exam 29.02.2020/02. Shopping List.js
+++ 04. Programming Fundamentals Mid Exam 29.02.2020/02. Shopping List.js
@@ -1,5 +1,60 @@
-function heartDelivery(input) {
-    let i;
+function shoppingList(input) {
+
+    let listOfItems = input.shift().split("!");
+
+    for (const item of input) {
+        if (item == "Go Shopping!"){
+            console.log(listOfItems.join(", "));
+            break;
+        }
+        let info = item.split(" ");
+        let command = info.shift();
+        let currItem = info[0];
+        switch (command){
+            case "Urgent": 
+                if (!listOfItems.includes(currItem)){
+                    listOfItems.unshift(currItem);
+                }
+                break;
+            case "Unnecessary":
+                if (listOfItems.includes(currItem)){
+                    listOfItems.splice(listOfItems.indexOf(currItem),1);
+                }
+                break;
+                // correct has 2 items - old && new.
+            case "Correct":
+                let oldItem = currItem, newItem = info[1];
+                if (listOfItems.includes(oldItem)){
+                    listOfItems.splice(listOfItems.indexOf(oldItem), 1, newItem);
+                }
+                break;
+            case "Rearrange":
+                if (listOfItems.includes(currItem)){
+                    let itemToRearrange = listOfItems.splice(listOfItems.indexOf(currItem), 1);
+                    listOfItems.push(itemToRearrange);
+                }
+                break;
+            default:
+                console.log("something went wrong ..")
+
+        }
+    }
 }
 
-heartDelivery();
+// shoppingList([
+//     "Tomatoes!Potatoes!Bread",
+//     "Unnecessary Milk",
+//     "Urgent Tomatoes",
+//     "Go Shopping!",
+// ]);
+
+shoppingList([
+    "Milk!Pepper!Salt!Water!Banana",
+    "Urgent Salt",
+    "Unnecessary Grapes",
+    "Correct Pepper Onion",
+    "Rearrange Grapes",
+    "Correct Tomatoes Potatoes",
+    "Go Shopping!",
+]);
+
