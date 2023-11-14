8b2e417a04092b0fdf91d9eaef0aaaad5e164fa5
iliyanPetrov <iliyan.petrov1986@gmail.com>
2023-11-14 23:17:56 +0200

    3rd task in progress

--- 04. Programming Fundamentals Mid Exam 29.02.2020/03. Heart Delivery.js
+++ 04. Programming Fundamentals Mid Exam 29.02.2020/03. Heart Delivery.js
@@ -1,5 +1,46 @@
 function heartDelivery(input) {
-    let i;
+    let nHood = input.shift().split("@").map(Number);
+    let currIndex = 0;
+    const housesLength = nHood.length; 
+    for (const item of input) {
+        if (item == "Love!"){
+            // LAST POSITION AND OUTCOME OF MISSION
+            return;
+        }
+        const [command, num] = item.split(" ");
+
+            // CHECK JUMP RANGE
+        if (currIndex + num > housesLength - 1){
+            currIndex = 0
+        } else {
+            currIndex += num;
+        }
+
+            // CHECK FOR 0
+        if (!nHood[currIndex] == 0){
+            nHood[currIndex] -= 2;
+            if (nHood[currIndex] == 0){
+                console.log(`Place ${currIndex} has Valentine's day.`);
+            }
+        } else {
+            console.log(`Place ${currIndex} already had Valentine's day.`)
+        }
+    }
 }
 
-heartDelivery();
+heartDelivery([
+    "10@10@10@2", 
+    "Jump 1", 
+    "Jump 2", 
+    "Love!"
+]);
+
+// heartDelivery([
+//     "2@4@2",
+//     "Jump 2",
+//     "Jump 2",
+//     "Jump 8",
+//     "Jump 3",
+//     "Jump 1",
+//     "Love!",
+// ]);
