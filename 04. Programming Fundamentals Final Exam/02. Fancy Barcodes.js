69205156b472f6a63ea5bef28ae837ea6d8bcd53
iliyanPetrov <iliyan.petrov1986@gmail.com>
2023-11-21 00:02:01 +0200

    second task ready (fancy Barcodes)

--- 04. Programming Fundamentals Final Exam/02. Fancy Barcodes.js
+++ 04. Programming Fundamentals Final Exam/02. Fancy Barcodes.js
@@ -1,4 +1,30 @@
 function fancyBarcodes(input) {
-    let i;
+    let initialNumber = +input.shift(); // what's that for??
+    const regex = /@#+[A-Z][a-zA-Z\d]{4,}[A-Z]@#+/g; // positive look behind second '@' (symbol for capital letter)
+
+    for (const line of input) {
+        let match = line.match(regex);
+        // console.log(match)
+        if (match == null) {
+            console.log("Invalid barcode");
+            continue;
+        }
+        // console.log(match[0])
+        const validItem = match[0].match(/\w+/g);
+        // console.log(validItem[0]);
+        let productGroup = validItem[0].match(/\d/g);
+        // console.log(productGroup)
+        if (productGroup == null) productGroup = '00';
+        else productGroup = productGroup.join("");
+        console.log(`Product group: ${productGroup}`);
+    }
 }
-fancyBarcodes();
+fancyBarcodes([
+    "6",
+    "@###Val1d1teM@###",
+    "@#ValidIteM@#",
+    "##InvaliDiteM##",
+    "@InvalidIteM@",
+    "@#Invalid_IteM@#",
+    "@#ValiditeM@#",
+]);
