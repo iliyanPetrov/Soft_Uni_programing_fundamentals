3f547ee6e02ecbd68882ae10f60b5a1d6585a6f5
iliyanPetrov <iliyan.petrov1986@gmail.com>
2023-11-25 11:52:30 +0200

    for now

--- 04. Programming Fundamentals Final Exam/03. Heroes of Code and Logic VII.js
+++ 04. Programming Fundamentals Final Exam/03. Heroes of Code and Logic VII.js
@@ -1,5 +1,53 @@
-function heroesOfCode() {
-    let i;
+function heroesOfCode(inputArr) {
+    // GET INPUT AND PREPARE LOGIC
+    const numHeroes = +inputArr.shift();
+    // console.log([numHeroes])
+
+    const maxHP = 100, maxMP = 200;
+    const heroesMap = new Map();
+
+    function processHero(times){
+        let n = times;
+        while (n !== 0){
+            //  heroName, stats
+            const [heroName, health, mana] = inputArr.shift().split(" ");
+            const stats = [+health, +mana];
+            heroesMap.set(heroName, stats);
+            n--;
+        }
+        console.table(heroesMap)
+    }
+    processHero(numHeroes);
+    // console.log(inputArr)
+
+    // PROCESS COMMANDS
+    // After the "End" command, we print the remaining living heroes.
+    for (const element of inputArr) {
+        console.log(element)
+    }
 }
 
-heroesOfCode();
+heroesOfCode([
+    "2",
+    "Solmyr 85 120",
+    "Kyrre 99 50",
+    "Heal - Solmyr - 10",
+    "Recharge - Solmyr - 50",
+    "TakeDamage - Kyrre - 66 - Orc",
+    "CastSpell - Kyrre - 15 - ViewEarth",
+    "End",
+]);
+
+// heroesOfCode([
+//     "4",
+//     "Adela 90 150",
+//     "SirMullich 70 40",
+//     "Ivor 1 111",
+//     "Tyris 94 61",
+//     "Heal - SirMullich - 50",
+//     "Recharge - Adela - 100",
+//     "CastSpell - Tyris - 1000 - Fireball",
+//     "TakeDamage - Tyris - 99 - Fireball",
+//     "TakeDamage - Ivor - 3 - Mosquito",
+//     "End",
+// ]);
